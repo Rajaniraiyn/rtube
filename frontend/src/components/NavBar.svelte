2f1072e0684693259a1094e23bb31e58e2498372
Rajaniraiyn <rajaniraiyn@gmai.com>
2022-09-11 20:59:29 +0530

    🩹 Fix video preview above navbar

--- frontend/src/components/NavBar.svelte
+++ frontend/src/components/NavBar.svelte
@@ -59,6 +59,7 @@
     -webkit-backdrop-filter: blur(0.5em);
     backdrop-filter: blur(0.5em);
     box-shadow: 0px 10px 15px -3px rgba(0, 0, 0, 0.1);
+    transform: translateZ(75em);
 
     --wails-draggable: drag;
   }
